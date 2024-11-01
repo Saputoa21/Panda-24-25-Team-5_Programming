b480a2349bd7e8e2fce6de195332862a6b82ebb2
Polina <pbogdanova2000@gmail.com>
2024-11-01 13:01:44 +0100

    -

---
+++ ex.2/Exercise2_Bogdanova.py
@@ -0,0 +1,91 @@
+from collections import deque
+
+
+def print_labyrinth(lab: list[str], path: list[tuple[int, int]] = None):
+
+    def replace_at_index(s: str, r: str, idx: int) -> str:
+        return s[:idx] + r + s[idx + len(r):]
+
+    n_columns = len(lab[0])
+    numbers = " " + "".join([str(i % 10) for i in range(n_columns)])
+
+    print(numbers)
+    for i, row in enumerate(lab):
+        if path:
+            for item in path:
+                if item[0] == i:
+                    row = replace_at_index(row, "X", item[1])
+
+        print(f"{i %10}{row}{i % 10}")
+
+    print(numbers)
+
+def prompt_integer(message: str) -> int:
+    text = input(message)
+
+    while not text.isdigit():
+        print("Only integers accepted!")
+        text = input(message)
+
+    return int(text)
+
+def prompt_user_for_location(name: str) -> tuple[int, int]:
+    row = prompt_integer(f"Row of {name} location: ")
+    column = prompt_integer(f"Column of {name} location: ")
+    return row, column
+
+
+def is_traversable(lab: list[str], location: tuple[int, int]) -> bool:
+    row, col = location
+    if 0 <= row < len(lab) and 0 <= col < len(lab[0]):
+        return lab[row][col] == ' '
+    return False
+
+def bfs(lab: list[str], start: tuple[int, int], end: tuple[int, int]) -> list[tuple[int, int]]:
+    queue = deque([[start]])
+    visited = set()
+
+    moves = [(0, 1), (0, -1), (1, 0), (-1, 0)]
+
+    while queue:
+        path = queue.popleft()
+        last = path[-1]
+
+        if last == end:
+            return path
+
+        if last not in visited:
+            visited.add(last)
+
+            for move in moves:
+                next_location = (last[0] + move[0], last[1] + move[1])
+
+                if is_traversable(lab, next_location):
+                    new_path = list(path)
+                    new_path.append(next_location)
+                    queue.append(new_path)
+
+    return []
+
+
+# Labyrinth represented as a list of strings
+labyrinth = [
+    "█████████████",
+    "█           █",
+    "█ █████ █████",
+    "█ █   █     █",
+    "█ ███ █ █████",
+    "█     █     █",
+    "█████████████"
+]
+
+
+print_labyrinth(labyrinth)
+
+start_location = prompt_user_for_location("start")
+end_location = prompt_user_for_location("end")
+
+# Find path using breadth-first search between start and end locations
+path = bfs(labyrinth, start_location, end_location)
+
+print_labyrinth(labyrinth, path)
