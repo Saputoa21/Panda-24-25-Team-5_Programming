9ce3f44965952d37620cc84772d8a19b6c6b731f
Sputoa21 <anastasiya.saputo@mail.ru>
2024-11-29 17:38:11 +0100

    My Version / Saputo

--- Exercise4.py
+++ Exercise4.py
@@ -1,1 +1,61 @@
 # Exercise 4
+
+from collections import Counter
+import string
+
+
+#Step 1
+song = """
+… So, so you think you can tell heaven from hell? 
+Blue skies from pain? 
+Can you tell a green field from a cold steel rail? 
+A smile from a veil? 
+Do you think you can tell? 
+… Did they get you to trade your heroes for ghosts? 
+Hot ashes for trees? 
+Hot air for a cool breeze? 
+Cold comfort for change? 
+Did you exchange a walk on part in the war 
+For a lead role in a cage? 
+… How I wish, how I wish you were here 
+We're just two lost souls swimming in a fish bowl 
+Year after year 
+Running over the same old ground, what have we found? 
+The same old fears, wish you were here 
+"""
+
+
+
+#with open('song.txt', 'w', encoding='utf-8'):
+    
+
+
+#Step 2
+
+# with open('song.txt', 'r', encoding='utf-8') as file:
+#     print(file)
+#
+
+
+# Step 3 Counting Letter Frequencies
+letter_counts = Counter(char.lower() for char in song if char.lower() in string.ascii_lowercase)
+
+non_alpha_counts = sum(1 for char in song if char not in string.ascii_letters)
+
+# Print the results
+print("==== Letter Frequencies Results ==== ")
+for letter, count in sorted(letter_counts.items()):
+    print(f"{letter}: {count}")
+
+print("\nNon-alphabet characters:", non_alpha_counts)
+
+# Step 4: Identifying Most and Least Frequently Used Characters
+
+most_common_5_char = letter_counts.most_common(5)
+least_common_5_char = letter_counts.most_common()[:-6:-1]
+
+for char, count in sorted(letter_counts.items()):
+    print()
+
+for char, count in sorted(letter_counts.items()):
+    print()
