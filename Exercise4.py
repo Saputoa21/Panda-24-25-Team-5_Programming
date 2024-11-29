57457da387096fa9b91476db9ad0c5233fc82615
Sputoa21 <anastasiya.saputo@mail.ru>
2024-11-29 19:25:28 +0100

    My Version / Saputo

--- Exercise4.py
+++ Exercise4.py
@@ -36,36 +36,36 @@ print("===== Contents of 'song.txt' ===== \n")
 print(content,"\n")
 
 # Step 3: Counting Letter Frequencies
-#
-# letter_counts = Counter(char.lower() for char in song if char.lower() in string.ascii_lowercase)
-#
-# non_alpha_counts = sum(1 for char in song if char not in string.ascii_letters)
-#
-# print("==== Letter Frequencies Results ==== \n")
-# for letter, count in sorted(letter_counts.items()):
-#     print(f"{letter}: {count}")
-#
-# print("\nNon-alphabet characters:", non_alpha_counts,"\n")
+
+letter_counts = Counter(char.lower() for char in song if char.lower() in string.ascii_lowercase)
+
+non_alpha_counts = sum(1 for char in song if char not in string.ascii_letters)
+
+print("==== Letter Frequencies Results ==== \n")
+for letter, count in sorted(letter_counts.items()):
+    print(f"{letter}: {count}")
+
+print("\nNon-alphabet characters:", non_alpha_counts,"\n")
 
 # Step 4: Identifying Most and Least Frequently Used Characters
 
-# most_common_5_char = letter_counts.most_common(5)
-#
-# least_common_5_char = letter_counts.most_common()[:-6:-1]
-#
-# unused_letters = [letter for letter in string.ascii_lowercase if letter not in letter_counts]
-#
-# print("===== Top 5 Least Frequently Used Characters =====\n")
-# for char, count in sorted(least_common_5_char):
-#     print(f"{char}: {count}\n")
-#
-# print("===== Top 5 Least Frequently Used Characters =====\n")
-# for char, count in sorted(most_common_5_char):
-#     print(f"{char}: {count}\n")
-#
-# print("===== Unused Alphabet Characters =====\n")
-# for letter in unused_letters:
-#     print(letter)
+most_common_5_char = letter_counts.most_common(5)
+
+least_common_5_char = letter_counts.most_common()[:-6:-1]
+
+unused_letters = [letter for letter in string.ascii_lowercase if letter not in letter_counts]
+
+print("===== Top 5 Least Frequently Used Characters =====\n")
+for char, count in sorted(least_common_5_char):
+    print(f"{char}: {count}\n")
+
+print("===== Top 5 Least Frequently Used Characters =====\n")
+for char, count in sorted(most_common_5_char):
+    print(f"{char}: {count}\n")
+
+print("===== Unused Alphabet Characters =====\n")
+for letter in unused_letters:
+    print(letter)
 
 #Step 5: Counting Words and Unique Word Frequencies
 
@@ -81,30 +81,30 @@ word_percentages = {word: (count / total_word_count) for word, count in word_fre
 
 print(" ===== Word Frequency Results =====\n")
 
-# unique_words = set()
-# for word, count in word_frequencies.items():
-#     print(f"{word}: {count}")
-#     unique_words.add(word)
-#
-# print("\nTotal unique words found in the lyrics: ", len(unique_words),"\n")
-#
-# table = [
-#     ["Word", "Occurencies", "Percentage (%)"],
-#     ["----------","-----------","---------------"]
-#         ]
-#
-# for word, count in word_frequencies.items():
-#     for word2, percent in word_percentages.items():
-#         if word == word2:
-#             table.append([word, count, percent])
-#         else:
-#             continue
-#
-# print("===== Word Types and Their Percentage of Total Content ===== \n")
-#
-# row_format = "{:<10} {:<11} {:<15}"
-# for row in table:
-#     print(row_format.format(*row))
+unique_words = set()
+for word, count in word_frequencies.items():
+    print(f"{word}: {count}")
+    unique_words.add(word)
+
+print("\nTotal unique words found in the lyrics: ", len(unique_words),"\n")
+
+table = [
+    ["Word", "Occurencies", "Percentage (%)"],
+    ["----------","-----------","---------------"]
+        ]
+
+for word, count in word_frequencies.items():
+    for word2, percent in word_percentages.items():
+        if word == word2:
+            table.append([word, count, percent])
+        else:
+            continue
+
+print("===== Word Types and Their Percentage of Total Content ===== \n")
+
+row_format = "{:<10} {:<11} {:<15}"
+for row in table:
+    print(row_format.format(*row))
 
 # Step 6: Identifying Top 10 Most Frequent Words and Their Percentage of Total
 
