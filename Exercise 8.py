8f1885123195163c69db8ba91e7260b8059a0702
Sputoa21 <anastasiya.saputo@mail.ru>
2024-12-17 13:48:04 +0100

    My Version / Saputo

--- Exercise 8.py
+++ Exercise 8.py
@@ -1,9 +1,7 @@
 #Part 1
 from gettext import install
-
 import requests
 import json
-import string
 
 sonnets = requests.get('https://poetrydb.org/author,title/Shakespeare;Sonnet')
 print(sonnets.text)
@@ -16,26 +14,24 @@ print(type(sonnets_dict))
 
 #Part 2
 
-import re
-
 class Sonnet:
     def __init__(self, sonnet):
         full_title = sonnet.get("title", " ")
         self.lines = sonnet.get("lines", [])
         self.linecount = sonnet.get("linecount", " ")
 
-        match = re.match(r"Sonnet (\d+):\s*(.*)", full_title)
-        if match:
-            self.id = int(match.group(1))
-            self.title = match.group(2)
+        parts = full_title.split(":")
+        sonnet_number_part = parts[0]  # "Sonnet 1"
+        self.title = parts[1].strip()  # "From fairest creatures we desire increase" - we use strip because there can be a whitespace beween two parts, e.g. Sonnet 1_'From ...'.
+        self.id = int(sonnet_number_part.split(" ")[1]) # we devide the "Sonnet 1" by space between the words and get "Sonnet" at index [0] and the number at index [1]
     def __str__(self):
         lines_print = "\n".join(self.lines)
-        return (f"Sonnet {self.id}: {self.title}"
+        return (f"Sonnet {self.id}: {self.title}\n"
                 f"{lines_print}\n")
     def __repr__(self):
         return f"Sonnet(id={self.id}, title='{self.title}', lines={self.lines})"
 
-
+# creating an instance of the class Sonnet
 sonnet_dict1 = sonnets_dict[0]
 sonnet1 = (Sonnet(sonnet_dict1))
 
