1943bfc02e059ba364e0bb2d91227110790562c9
Sputoa21 <anastasiya.saputo@mail.ru>
2024-12-18 12:39:09 +0100

    My Version / Saputo

--- Exercise 8.py
+++ Exercise 8.py
@@ -1,18 +1,15 @@
-#Part 1
 from gettext import install
 import requests
 import json
+import string
 
 sonnets = requests.get('https://poetrydb.org/author,title/Shakespeare;Sonnet')
 print(sonnets.text)
 print(type(sonnets.text))
 
-
 sonnets_dict = json.loads(sonnets.text)
 print(sonnets_dict[0])
 print(type(sonnets_dict))
-
-#Part 2
 
 class Sonnet:
     def __init__(self, sonnet):
@@ -22,7 +19,7 @@ class Sonnet:
 
         parts = full_title.split(":")
         sonnet_number_part = parts[0]  # "Sonnet 1"
-        self.title = parts[1].strip()  # "From fairest creatures we desire increase" - we use strip because there can be a whitespace beween two parts, e.g. Sonnet 1_'From ...'.
+        self.title = parts[1].strip()  # "From fairest creatures we desire increase" - we use strip because there can be a whitespace between two parts, e.g. Sonnet 1_'From ...'.
         self.id = int(sonnet_number_part.split(" ")[1]) # we devide the "Sonnet 1" by space between the words and get "Sonnet" at index [0] and the number at index [1]
     def __str__(self):
         lines_print = "\n".join(self.lines)
@@ -31,6 +28,16 @@ class Sonnet:
     def __repr__(self):
         return f"Sonnet(id={self.id}, title='{self.title}', lines={self.lines})"
 
+    def tokenize(self) -> list[str]:
+        tokens = []
+        for line in self.lines:
+            processed_line = str(line).lower().split(" ")
+            for token in processed_line:
+                if token != ():
+                    tokens = tokens.append(processed_line)
+                    return tokens
+
+
 # creating an instance of the class Sonnet
 sonnet_dict1 = sonnets_dict[0]
 sonnet1 = (Sonnet(sonnet_dict1))
@@ -38,4 +45,4 @@ sonnet1 = (Sonnet(sonnet_dict1))
 print(sonnet1)
 print(repr(sonnet1))
 
-#Part 3
+print(sonnet1.tokenize())
