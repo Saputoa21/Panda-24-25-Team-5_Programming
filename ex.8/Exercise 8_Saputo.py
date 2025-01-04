fb09d8047f2987a73953844198eae7be06dd8a72
Sputoa21 <anastasiya.saputo@mail.ru>
2025-01-04 22:31:19 +0100

    My Version / Saputo

--- ex.8/Exercise 8_Saputo.py
+++ ex.8/Exercise 8_Saputo.py
@@ -87,40 +87,61 @@ class Index(dict[str, set[int]]):
                 matching_sonnets.append(document)
         return matching_sonnets
 
+#Interface
+while True:
+    sonnet_list = [Sonnet(sonnet) for sonnet in sonnets]
+    index = Index(sonnet_list)
+    inverted_index = index.get_inverted_index()
+    user_query = input(f"Search for sonnets ('q' to quit)> ")
+    if user_query == "q":
+        break
+    new_user_query = Query(user_query)
+    matching_sonnets = index.search(new_user_query)
+    if len(matching_sonnets) != 0:
+        number_of_matching_sonnets = len(matching_sonnets)
+        matching_sonnets_ids = []
+        for matching_sonnet in matching_sonnets:
+            matching_sonnets_ids.append(matching_sonnet.id)
+        print("Reading sonnets... ")
+        print(f"Your search for '{user_query}' matched {number_of_matching_sonnets} sonnets ({matching_sonnets_ids}):")
+        for matching_sonnet in matching_sonnets:
+            print(matching_sonnet)
+    else:
+        print(f"Unfortunaltelly, no sonnet matched your query {user_query}.")
+
+
+
+#Testing the output of each step
+
 # Creating an instance of the class Sonnet
-sonnet1 = (Sonnet(sonnets[0]))
+#sonnet1 = (Sonnet(sonnets[0]))
+# print(sonnet1.id)
+# print(repr(sonnet1))
+# print(sonnet1.tokenize(stemmer))
 
-print(sonnet1.id)
-print(repr(sonnet1))
-print(sonnet1.tokenize(stemmer))
+#output without stemmer
+#['look', 'in', 'thy', 'glass', 'and', 'tell', 'the', 'face', 'thou', 'viewest', 'now', 'is', 'time', 'that', 'should', 'form', 'another', 'whose', 'fresh', 'repair', 'if', 'not', 'renewest', 'dost', 'beguile', 'world', 'unbless', 'some', 'mother', 'for', 'where', 'she', 'so', 'fair', 'uneard', 'womb', 'disdains', 'tillage', 'of', 'husbandry', 'or', 'who', 'he', 'fond', 'will', 'be', 'tomb', 'his', 'selflove', 'to', 'stop', 'posterity', 'art', 'mothers', 'thee', 'calls', 'back', 'lovely', 'april', 'her', 'prime', 'through', 'windows', 'thine', 'age', 'shalt', 'see', 'despite', 'wrinkles', 'this', 'golden', 'but', 'live', 'rememberd', 'die', 'single', 'image', 'dies', 'with']
 
-"""
-output without stemmer
-['look', 'in', 'thy', 'glass', 'and', 'tell', 'the', 'face', 'thou', 'viewest', 'now', 'is', 'time', 'that', 'should', 'form', 'another', 'whose', 'fresh', 'repair', 'if', 'not', 'renewest', 'dost', 'beguile', 'world', 'unbless', 'some', 'mother', 'for', 'where', 'she', 'so', 'fair', 'uneard', 'womb', 'disdains', 'tillage', 'of', 'husbandry', 'or', 'who', 'he', 'fond', 'will', 'be', 'tomb', 'his', 'selflove', 'to', 'stop', 'posterity', 'art', 'mothers', 'thee', 'calls', 'back', 'lovely', 'april', 'her', 'prime', 'through', 'windows', 'thine', 'age', 'shalt', 'see', 'despite', 'wrinkles', 'this', 'golden', 'but', 'live', 'rememberd', 'die', 'single', 'image', 'dies', 'with']
-"""
+#output with stemmer
+#['look', 'in', 'thy', 'glass', 'and', 'tell', 'the', 'face', 'thou', 'viewest', 'now', 'is', 'time', 'that', 'should', 'form', !'anoth', 'whose', 'fresh', 'repair', 'if', 'not', 'renewest', 'dost', !'beguil', 'world', 'unbless', 'some', 'mother', 'for', 'where', 'she', 'so', 'fair', 'uneard', 'womb', !'disdain', !'tillag', 'of', !'husbandri', 'or', 'who', 'he', 'fond', 'will', 'be', 'tomb', !'hi', !'selflov', 'to', 'stop', !'poster', 'art', !'mother', 'thee', !'call', 'back', 'love', 'april', 'her', 'prime', 'through', !'window', 'thine', !'ag', 'shalt', 'see', !'despit', !'wrinkl', !'thi', 'golden', 'but', 'live', 'rememberd', 'die', !'singl', !'imag', 'di', 'with']
 
-"""
-output with stemmer  
-['look', 'in', 'thy', 'glass', 'and', 'tell', 'the', 'face', 'thou', 'viewest', 'now', 'is', 'time', 'that', 'should', 'form', !'anoth', 'whose', 'fresh', 'repair', 'if', 'not', 'renewest', 'dost', !'beguil', 'world', 'unbless', 'some', 'mother', 'for', 'where', 'she', 'so', 'fair', 'uneard', 'womb', !'disdain', !'tillag', 'of', !'husbandri', 'or', 'who', 'he', 'fond', 'will', 'be', 'tomb', !'hi', !'selflov', 'to', 'stop', !'poster', 'art', !'mother', 'thee', !'call', 'back', 'love', 'april', 'her', 'prime', 'through', !'window', 'thine', !'ag', 'shalt', 'see', !'despit', !'wrinkl', !'thi', 'golden', 'but', 'live', 'rememberd', 'die', !'singl', !'imag', 'di', 'with']
-"""
-
-# We found it interseting that the stemmer did only little changes on the words and is not suitable for stemming such old texts, which had own grammar, e.g. the ending -est for second-person singular simple present indicative of view)
+# Note: We found it interseting that the stemmer did only little changes on the words and is not suitable for stemming such old texts, which had own grammar, e.g. the ending -est for second-person singular simple present indicative of view)
 # also "thy" is not changes although there is a method in the stemmer class for converting "y" to "i"
 
 # Creating list of instances of the class Sonnet
-sonnet_list = [Sonnet(sonnet) for sonnet in sonnets]
+# sonnet_list = [Sonnet(sonnet) for sonnet in sonnets]
 
 # Getting inverted index
-index = Index(sonnet_list)
-inverted_index = index.get_inverted_index()
-print(inverted_index)
+# index = Index(sonnet_list)
+# inverted_index = index.get_inverted_index()
+# print(inverted_index)
 
 # Creating an instance of Query class:
-query = Query("love hate")
-print(query)
-print(query.tokenize(stemmer))
+# query = Query("love hate")
+# print(query)
+# print(query.tokenize(stemmer))
 
 # Search the index with the query "love" and "hate"
-matching_sonnets = index.search(query)
-for matching_sonnet in matching_sonnets:
-    print(matching_sonnet)  # [35, 40, 89, 90, 117, 124, 129, 142, 145, 149, 150, 152]
+# matching_sonnets = index.search(query)
+# for matching_sonnet in matching_sonnets:
+#     print(matching_sonnet)  # [35, 40, 89, 90, 117, 124, 129, 142, 145, 149, 150, 152]
